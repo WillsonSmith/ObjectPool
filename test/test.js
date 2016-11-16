fb1c160a4d1072aeca7a3539686e7406f410c8bb
Willson Smith <me@willsonsmith.com>
2016-11-15 23:32:07 -0500

    use arrow functions for tests

--- test/test.js
+++ test/test.js
@@ -2,51 +2,51 @@ var ObjectPool = require('../lib/objectpool').default;
 
 var assert = require('assert');
 
-describe('ObjectPool', function() {
+describe('ObjectPool', () => {
   var pool;
   var allocateWithNumberPool;
 
-  beforeEach(function() {
+  beforeEach(() => {
     pool = new ObjectPool();
     allocateWithNumberPool = new ObjectPool(5);
   });
 
-  afterEach(function() {
+  afterEach(() => {
     pool = null;
     allocateWithNumberPool = null;
   });
 
-  describe('instantiation', function() {
+  describe('instantiation', () => {
 
-    it('should allocate the number of objects it is told or zero', function() {
+    it('should allocate the number of objects it is told or zero', () => {
       assert.equal(allocateWithNumberPool.status.totalAllocated, 5);
       assert.equal(allocateWithNumberPool.status.totalFree, 5);
     });
 
   });
 
-  describe('methods', function() {
+  describe('methods', () => {
 
-    it('adds an allocated item to its status when allocating without any available objects', function() {
+    it('adds an allocated item to its status when allocating without any available objects', () => {
       var item = pool.alloc();
       assert.equal(pool.status.totalAllocated, 1);
       assert.equal(pool.status.totalFree, 0);
     });
 
-    it('uses an existing object if available', function() {
+    it('uses an existing object if available', () => {
       allocateWithNumberPool.alloc();
       assert.equal(allocateWithNumberPool.status.totalAllocated, 5);
       assert.equal(allocateWithNumberPool.status.totalFree, 4);
     });
 
-    it('frees an object when calling free', function() {
+    it('frees an object when calling free', () => {
       var object = allocateWithNumberPool.alloc();
       assert.equal(allocateWithNumberPool.status.totalFree, 4);
       allocateWithNumberPool.free(object);
       assert.equal(allocateWithNumberPool.status.totalFree, 5);
     });
 
-    it('clears all unallocated objects when calling collect', function() {
+    it('clears all unallocated objects when calling collect', () => {
       var object = allocateWithNumberPool.alloc();
       allocateWithNumberPool.collect();
       assert.equal(allocateWithNumberPool.status.totalAllocated, 1);
